53a709effbcef6558cd9e92dfdedf5218d5b61ed
xenon-xenon <KhodeXenon@gmail.com>
2020-10-25 10:38:25 +0330

    a new line added to usage message

--- main/design/arg_options.py
+++ main/design/arg_options.py
@@ -7,7 +7,7 @@ from sys import argv
 scripts_name_all = [] # this list contains all of scripts name for existence checking in --script script_name parameter
 
 def parse_args():
-    parser = argparse.ArgumentParser(usage='sudo python3 %(prog)s --script script_name [options]' + '\n help: sudo python3 %(prog)s --help')
+    parser = argparse.ArgumentParser(usage='sudo python3 %(prog)s --script script_name [options]' + '\n help: sudo python3 %(prog)s --help\n')
     parser.add_argument('--script',help='Script Name To Choose',metavar='script_name')
     parser.add_argument('--show-scripts',help='Show Scripts Names',action='store_true')
     parser.add_argument('--show-examples',help='Show Some Examples',action='store_true')
