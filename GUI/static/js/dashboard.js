06dfe23ac6c3abdd68bb1d75fa9747f5f83d9314
Mohammad Amin Nasiri <Khodexenon@gmail.com>
2020-11-06 12:33:05 +0330

    static folder files updated

--- GUI/static/js/dashboard.js
+++ GUI/static/js/dashboard.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "25%";
+  document.getElementById("myNav").style.width = "20%";
 }
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
