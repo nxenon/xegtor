dc01b43462b8287cba30dccd4940c909d4953f78
Mohammad Amin Nasiri <Khodexenon@gmail.com>
2020-11-28 13:56:50 +0330

    perform_attack page now showes entered values

--- GUI/functions/perform_attack.py
+++ GUI/functions/perform_attack.py
@@ -5,6 +5,43 @@ this script is for /perform_attack page in which you can perform and start attac
 '''
 
 from flask import render_template,request,redirect
+import json
+from modules.get_argument_default_value import get_default_value
+
+def run_attack(script_name,arguments_dict):
+    pass
+
+def show_arguments():
+    content = ''
+    file = open('GUI/functions/arguments.json', 'r')
+    arguments_in_file = json.load(file)
+    script_name = request.args.get('script')
+    content += '<br><br><h2 style="text-align : center">' + script_name + '</h2>' # show script name in top of the page
+
+    url_args_names = list(request.args)
+    content += '<br><br>'
+    arguments_and_values = {} # store arguments and their values into a dictionary to pass in run_attack function
+    for arg in url_args_names :
+        if arg != 'script' :
+            argument_value = request.args.get(arg)
+            if argument_value == '':
+                default_value = get_default_value(script_name=script_name ,argument=arg)
+                argument_value = default_value[1]
+
+            content += '<h4 style="text-align : center">' + arg + ' : ' + argument_value + '</h4>'
+            arguments_and_values[arg.lower()] = argument_value
+
+    content += '<br><br>'
+    run_attack(script_name=script_name ,arguments_dict=arguments_and_values)
+    return content
 
 def perform_attack():
-    return 'test'
+    page = render_template('dashboard.html')
+    if (request.cookies.get('logged_in') == 'yes'):
+        page = page.replace('{to_replace_username}', request.cookies.get('user').title())
+    else:
+        return redirect('/login', code=302)
+
+    content = show_arguments()
+    page = page.replace('{to_replace_text}',content)
+    return page
