c88fb1d1594bb58b447a371b97f79080179457d2
Mohammad Amin Nasiri <Khodexenon@gmail.com>
2020-12-03 13:14:58 +0330

    edit notes

--- main/design/notes.py
+++ main/design/notes.py
@@ -3,7 +3,7 @@
 from colorama import Fore
 
 note_1 = Fore.RED + '*** ' + Fore.RESET + 'You have to run this script with root privileges' + Fore.RED + ' ***' + Fore.RESET
-note_2 = Fore.RED + '*** ' + Fore.RESET + 'You should have enabled ip forwarding on your machine' + Fore.RED + ' ***' + Fore.RESET
+note_2 = Fore.RED + '*** ' + Fore.RESET + 'You should have enabled ip forwarding on your machine (for MITM attacks)' + Fore.RED + ' ***' + Fore.RESET
 
 notes = note_1 + '\n' + note_2
 
