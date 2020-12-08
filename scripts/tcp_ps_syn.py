35a1202460fd461777b64e0853b5a44be682ea29
Mohammad Amin Nasiri <Khodexenon@gmail.com>
2020-12-08 21:38:12 +0330

    change log file name

--- scripts/tcp_ps_syn.py
+++ scripts/tcp_ps_syn.py
@@ -9,10 +9,18 @@ import socket
 from threading import Thread
 from time import sleep
 from modules.logger import Logger
+from os.path import isfile
 
-log_file_path = 'logs/xegtor.log'
-logger = Logger(log_file=log_file_path ,filemode='a')
-logger.check_logs()
+logs_dir = 'logs/'
+SCRIPT_NAME = 'tcp_ps_syn.py'
+log_file_name = 'tcp_ps_syn_py.log'
+log_file_path = logs_dir + log_file_name
+
+if isfile(log_file_path) :
+    logger = Logger(log_file=log_file_path, filemode='a')
+else:
+    logger = Logger(log_file=log_file_path, filemode='w')
+    logger.add_log_header(script_name=SCRIPT_NAME)
 
 class TcpSynPortScanner():
     def __init__(self,target_ip,port,timeout):
