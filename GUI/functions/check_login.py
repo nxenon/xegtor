502074608778b78a9724f04a739944468d718695
Mohammad Amin Nasiri <Khodexenon@gmail.com>
2020-12-15 22:51:59 +0330

    check_login.py integrated with get_config_value module

--- GUI/functions/check_login.py
+++ GUI/functions/check_login.py
@@ -4,8 +4,8 @@
 this script checks the login information
 '''
 
-import json
 from flask import redirect,make_response
+from modules.get_config_value import get_config_value
 
 class CheckLogin:
     def __init__(self,user,user_pass):
@@ -24,20 +24,21 @@ class CheckLogin:
             return self.check_login()
 
     def read_login(self):
-        file = open('GUI/config.json','r')
-        data = json.load(file)
-        self.username_in_app = data['credentials'][0]['user']
-        self.password_in_app = data['credentials'][0]['pass']
+        self.username_in_app = get_config_value(main_key='credentials' ,key_in_list='user')
+        self.password_in_app = get_config_value(main_key='credentials' ,key_in_list='pass')
         if ((self.username_in_app) and (self.password_in_app)):
             pass
         else:
+            print('[!] web login failed')
             return redirect('/login?error=nof') # nof is for no credential found in config.json
 
     def check_login(self):
         if ((self.username_in_app.lower() == self.username_in_login.lower()) and (self.password_in_app == self.password_in_login)):
+            print('[+] web login succeeded')
             response = make_response(redirect('/dashboard',code=302))
             response.set_cookie('user',self.username_in_app,max_age=3600)
             response.set_cookie('logged_in','yes',max_age=3600)
             return response
         else:
+            print('[!] web login failed')
             return redirect('login?error=lf',code=302)
